2eee8fda061592946709709fccf067e21d1eeaaf
joshuascharley <joshuacharley87@gmail.com>
2019-09-07 16:33:14 -0700

    implemented firebase utitls

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -7,18 +7,43 @@ const config = {
     authDomain: "salbuy-db.firebaseapp.com",
     databaseURL: "https://salbuy-db.firebaseio.com",
     projectId: "salbuy-db",
-    storageBucket: "",
+    storageBucket: "salbuy-db.appspot.com",
     messagingSenderId: "238779676396",
     appId: "1:238779676396:web:91338968f4601eaafa97c9"
-  };
+};
 
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
 
-  export const auth = firebase.auth();
-//   export const firestore = firestore.firestore();
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
 
-  const provider = new firebase.auth.GoogleAuthProvider();
-  provider.setCustomParameters({prompt: 'select_account'});
-  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
 
-  export default firebase;
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
